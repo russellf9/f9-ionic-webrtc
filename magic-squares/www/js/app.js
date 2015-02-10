963fa2186366e03d9a85c5f4058250a780032b90
russellf9 <russell@factornine.co.uk>
2015-02-10 09:28:37 +0000

    * Documentation - Code comments have been improved.

--- magic-squares/www/js/app.js
+++ magic-squares/www/js/app.js
@@ -1,24 +1,10 @@
-// Ionic Starter App
+// Magic Squares App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
 angular.module('magicsquares', ['ionic', 'ngDragDrop', 'angular.filter', 'magicsquares.controllers', 'magicsquares.services'])
 
     .run(function(_, $ionicPlatform) {
         $ionicPlatform.ready(function() {
             StatusBar.hide();
-            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-            // for form inputs)
-//            if (window.cordova && window.cordova.plugins.Keyboard) {
-//                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-//            }
-//            if (window.StatusBar) {
-//                // org.apache.cordova.statusbar required
-//                StatusBar.styleDefault();
-//            }
         });
     })
 
