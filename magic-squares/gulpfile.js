a245d40c0a0e143adb716db1aaf5c67115038a9e
russellf9 <russell@factornine.co.uk>
2015-02-11 17:57:57 +0000

    * Refactoring

--- magic-squares/gulpfile.js
+++ magic-squares/gulpfile.js
@@ -5,7 +5,8 @@ var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
-var sh = require('shelljs');
+var sh = require('shelljs'),
+    git = require('gulp-git');
 
 var paths = {
   sass: ['./scss/**/*.scss']
@@ -47,4 +48,29 @@ gulp.task('git-check', function(done) {
     process.exit(1);
   }
   done();
+});
+
+// Create a new git branch
+// Uses the default name as: dev-{YYMMDD}
+gulp.task('branch', function() {
+    var today = new Date(),
+        month = String((today.getMonth() + 1 >= 10) ? (today.getMonth() + 1) : ('0' + String(today.getMonth() + 1))),
+        date = String((today.getDate() >= 10) ? (today.getDate()) : ('0' + (today.getDate()))),
+        year = String(today.getFullYear()),
+        shortYear = year.substr(year.length - 2, 2),
+        dateString = shortYear + month + date,
+        branchName = 'dev-' + dateString;
+
+    git.checkout(branchName, {args: '-b'}, function(err) {
+        if (err) {
+            throw err;
+        } else {
+            console.log('Git branch - ', branchName, ' created.');
+        }
+    });
+});
+
+
+gulp.task('test', function() {
+    console.log('test!');
 });
