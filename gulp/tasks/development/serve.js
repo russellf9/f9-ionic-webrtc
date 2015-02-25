3b79a7501d83dacb1a293ae3f091571651cf5ac9
russellf9 <russell@factornine.co.uk>
2015-02-25 14:39:19 +0000

    #2-gulp - The `serve` task has been normalised.

--- gulp/tasks/development/serve.js
+++ gulp/tasks/development/serve.js
@@ -1,36 +1,18 @@
 var gulp = require('gulp'),
     config = require('../../config'),
-    express = require('express'),
     path = require('path'),
-    connectLr = require('connect-livereload'),
-    open = require('open'),
-    connect = require('gulp-connect'),
-    args = require('yargs')
-        .alias('e', 'emulate')
-        .alias('b', 'build')
-        .alias('r', 'run')
-        .default('build', false)
-        .default('port', 9000)
-        .argv;
+    connect = require('gulp-connect');
 
-// NOTE - HAVE TO BUILD FIRST!
-
-// start local express server
+// start local server
 gulp.task('serve', function() {
 
-    var build = args.build || args.emulate || args.run,
-        port = 9000,
-
+    var build = gulp.args.build || gulp.args.emulate || gulp.args.run,
+        port = gulp.args.port || 9000,
         targetDir = path.resolve(build ? 'www' : '.tmp');
 
     connect.server({
         root: targetDir,
+        port: port,
         livereload: true
     });
-
-    //express()
-    //    .use(!build ? connectLr() : function(){})
-    //    .use(express.static(targetDir))
-    //    .listen(port);
-    //open('http://localhost:' + port + '/', 'Google Chrome');
 });
