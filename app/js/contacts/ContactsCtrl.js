1528b440fd4b7e0894acada13e9a1a0a913f793f
russellf9 <russell@factornine.co.uk>
2015-03-17 11:57:03 +0000

    Contacts UI - Trello #56-contacts-ui
    A UI has to be implemented to show the array of contacts
    
    - Keep the code tidy

--- app/js/contacts/ContactsCtrl.js
+++ app/js/contacts/ContactsCtrl.js
@@ -5,16 +5,11 @@ angular.module('f9-webrtc')
     .controller('ContactsCtrl', ['$scope', '$timeout', 'ContactsService', function($scope, $timeout, ContactsService) {
         $scope.contacts = ContactsService.onlineUsers;
 
-
-        $scope.title = ' `s Contacts';
-
         // watch the service for updates to the user status
         $scope.$watch(ContactsService.getUsers, function(newValue, oldValue, scope) {
             console.log('\n** !Contacts Updated ', newValue);
             $timeout(function() {
                 $scope.currentUser = newValue.currentUser;
-
-               // $scope.title = $scope.currentUser + ' `s Contacts';
             }, 20);
         });
 
